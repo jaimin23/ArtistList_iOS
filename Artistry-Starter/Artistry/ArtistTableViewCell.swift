db3b5c3b1f33f17ae2374e42f49756e19e301703
Xcode User <xcode@Fasts-MacBook-Pro-2.local>
2016-11-14 11:43:08 -0500

    Inital Commit of the List project

---
+++ Artistry-Starter/Artistry/ArtistTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistTableViewCell.swift
+//  Artistry
+//
+//  Created by Xcode User on 2016-11-14.
+//  Copyright © 2016 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell {
+  
+  @IBOutlet weak var bioLabel: UILabel!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var artistImageView: UIImageView!
+
+}
---
+++ Artistry-Starter/Artistry/WorkTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  WorkTableViewCell.swift
+//  Artistry
+//
+//  Created by Xcode User on 2016-11-14.
+//  Copyright © 2016 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class WorkTableViewCell: UITableViewCell {
+  @IBOutlet weak var workImageView: UIImageView!
+  @IBOutlet weak var workTitleLabel: UILabel!
+  @IBOutlet weak var moreInfoTextView: UITextView!
+
+}
